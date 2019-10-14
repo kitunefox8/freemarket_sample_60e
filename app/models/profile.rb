ba419ba1abc4dbb80d5b351d443c04cc9e5e26e4
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-14 14:52:13 +0900

    profileモデルにバリデーション追加
    アソシエーション編集
    バリデーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,19 @@
 class Profile < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
+
+  tel_check = /^\d{11}$/
+  zipcode_check = /^\d{3}[-]\d{4}$/
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_kana, presence: true
+  validates :last_kana, presence: true
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
+  validates :tel, format{ with: tel_check }
+  validates :zipcode, presence: true, format{ with: zipcode_check }
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :district, presence: true
+  validates :building
 end
