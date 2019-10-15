a0c64a0d5306c19a613894c7314e93e41f7dfd08
TAKAHIRO IWATA <msm_monkey@icloud.com>
2019-10-15 18:54:31 +0900

    productsコントローラmigrate修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,33 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+    @product.build_category
+    @product.build_status
+    @product.build_brand
+  end
+
+  def show
+  end
+
+  def create
+    @product = Product.new(create_params)
+    if @product.save
+      redirect_to new_product_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def create_params
+    params.require(:product).permit(
+      :name, :price, :description, :exposition, :delivery_fee, :shipping_area, :shipping_days, :saller_id,
+      category_attributes: [:id, :name],
+      status_attributes: [:id, :name],
+      brand_attributes: [:id, :name]
+    )
+  end
 end
--- db/migrate/20191015075424_add_delivery_fee_to_products.rb
+++ db/migrate/20191015075424_add_delivery_fee_to_products.rb
@@ -3,7 +3,6 @@ class AddDeliveryFeeToProducts < ActiveRecord::Migration[5.2]
     add_column :products, :delivery_fee, :integer
     add_column :products, :shipping_area, :integer
     add_column :products, :shipping_days, :integer
-    add_column :products, :saller_id, :integer
     add_column :products, :category_id, :integer
     add_column :products, :status_id, :integer
     add_column :products, :brand_id, :integer
