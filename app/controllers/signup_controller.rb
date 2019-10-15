51fba9a965e52018d7b6fa51df8146271f03d481
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-15 10:23:59 +0900

    signup_controller編集
    sessionにparamsデータ保存
    user,profileのストリングパラメータ記述

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -3,4 +3,48 @@ class SignupController < ApplicationController
     @user = User.new
     @profile = Profile.new
   end
+  def session2
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+
+    session[:first_name] = profile_params[:first_name]
+    session[:last_name] = profile_params[:last_name]
+    session[:first_kana] = profile_params[:first_kana]
+    session[:last_kana] = profile_params[:last_kana]
+    session[:birth_year] = profile_params[:birth_year]
+    session[:birth_month] = profile_params[:birth_month]
+    session[:birth_day] = profile_params[:birth_day]
+    
+    @profile = User.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :name
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
+  def profile_params
+    params.require(:profile).permit(
+      :first_name, 
+      :last_name, 
+      :first_kana, 
+      :last_kana, 
+      :birth_year, 
+      :birth_month, 
+      :birth_day,
+      :tel,
+      :zipcode,
+      :prefecture,
+      :city,
+      :district,
+      :building
+  )
+  end 
 end
