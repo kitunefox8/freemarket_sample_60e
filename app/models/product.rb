a95686d372c8619668540fbe43413c06cfb7407f
TAKAHIRO IWATA <msm_monkey@icloud.com>
2019-10-15 18:36:04 +0900

    モデル修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
 has_many :comments
 has_many :likes
-belongs_to :order
-
+belongs_to :order, optional: true
+belongs_to :category
+belongs_to :status
+belongs_to :brand
+accepts_nested_attributes_for :category
+accepts_nested_attributes_for :status
+accepts_nested_attributes_for :brand
 end
