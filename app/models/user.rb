07930bd61d1f9d13daef27cf3b19670c4277b150
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-15 13:37:34 +0900

    userモデルに関連付け
    userモデルにprofileとcreditcardを関連付け
    accepts_nested_attributes_forを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,12 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :sns_credentials
-  has_one :creditcard
   # has_one :creditcard, dependent: :destory
-  has_one :profile
+  has_one :creditcard
+  accepts_nested_attributes_for :creditcard
   # has_one :profile, dependent: :destory
+  has_one :profile
+  accepts_nested_attributes_for :profile
 
   email_check = /\A[^@\s]+@[^@\s]+\z/
   validates :email,                 presence: true, uniqueness: { case_sensitive: false }, format: { with: email_check }
