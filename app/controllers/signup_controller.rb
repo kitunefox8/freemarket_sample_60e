27fe39e71d289a826eea2a0472169af937dbbc81
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-16 13:10:37 +0900

    signup_controllerコメントアウト削除

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -54,22 +54,6 @@ class SignupController < ApplicationController
       email: session[:email],
       password: session[:password],
       password_confirmation: session[:password_confirmation]
-
-      # profile: session[:first_name],
-      # last_name: session[:last_name],
-      # first_kana: session[:first_kana],
-      # last_kana: session[:last_kana],
-      # birth_year: session[:birth_year],
-      # birth_month: session[:birth_month],
-      # birth_day: session[:birth_day],
-      # zipcode: session[:zipcode],
-      # city: session[:city],
-      # district: session[:district],
-      # buiding: session[:buiding],
-      # credit_number: session[:credit_number],
-      # validity_month: session[:validity_month],
-      # validity_day: session[:validity_day],
-      # security_number: session[:security_number]
     )
     @profile = @user.build_profile(
       first_name: session[:first_name],
@@ -98,65 +82,19 @@ class SignupController < ApplicationController
       sign_in User.find(session[:id])
       redirect_to root_path
     else
-      redirect_to signup_index_path
-    end
+      redirect_to session0_signup_index_path
     end
   end
 
   private
   def user_params
     params.require(:user).permit(
-      :nickname,
-      :email,
-      :password,
-      :password_confirmation,
-      profile: [:first_name, :last_name, :first_kana, :last_kana, :birth_year, :birth_month, :birth_day, :tel, :zipcode, :prefecture, :city, :district, :building],
+      :nickname, :email, :password, :password_confirmation,
+      profile: [:first_name, :last_name, :first_kana, :last_kana,
+      :birth_year, :birth_month, :birth_day, :tel,
+      :zipcode, :prefecture, :city, :district, :building],
       creditcard: [:credit_number, :validity_month, :validity_day, :security_number]
-
-      # profile_attributes: [:id, :first_name],
-      # profile_attributes: [:id, :last_name],
-      # profile_attributes: [:id, :first_kana],
-      # profile_attributes: [:id, :last_kana],
-      # profile_attributes: [:id, :birth_year],
-      # profile_attributes: [:id, :birth_month],
-      # profile_attributes: [:id, :birth_day],
-      # profile_attributes: [:id, :tel],
-      # profile_attributes: [:id, :zipcode],
-      # profile_attributes: [:id, :city],
-      # profile_attributes: [:id, :district],
-      # profile_attributes: [:id, :buiding],
-      # creditcard_attributes: [:id, :credit_number],
-      # creditcard_attributes: [:id, :validity_month],
-      # creditcard_attributes: [:id, :validity_day],
-      # creditcard_attributes: [:id, :security_number]
-
     )
   end
-
-  # def profile_params
-  #   params.require(:profile).permit(
-  #     :first_name, 
-  #     :last_name, 
-  #     :first_kana, 
-  #     :last_kana, 
-  #     :birth_year, 
-  #     :birth_month, 
-  #     :birth_day,
-  #     :tel,
-  #     :zipcode,
-  #     :prefecture,
-  #     :city,
-  #     :district,
-  #     :building
-  # )
-  # end
-
-  # def creditcard_params
-  #   params.require(:creditcard).permit(
-  #     :credit_number,
-  #     :validity_month,
-  #     :validity_day,
-  #     :security_number
-  #   )
-  # end
+  
 end
