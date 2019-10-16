f7f0e4e5b07166ccd924cfea6f6d45ddb0e14c63
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-16 15:08:37 +0900

    バリデーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
 
-  # tel_check = /^\d{11}$/
-  # zipcode_check = /^\d{3}[-]\d{4}$/
+  tel_check = /\A\d{11}\z/
+  zipcode_check = /\A\d{3}[-]\d{4}\z/
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_kana, presence: true
@@ -10,9 +10,8 @@ class Profile < ApplicationRecord
   validates :birth_year, presence: true
   validates :birth_month, presence: true
   validates :birth_day, presence: true
-  # validates :tel, format: { with: tel_check }
-  validates :zipcode, presence: true
-  # validates :zipcode, presence: true, format:{ with: zipcode_check }
+  validates :tel, format: { with: tel_check }
+  validates :zipcode, presence: true, format:{ with: zipcode_check }
   validates :prefecture, presence: true
   validates :city, presence: true
   validates :district, presence: true
