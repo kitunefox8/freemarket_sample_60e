1d5f5338ee96a99aed727d0d2655b18cb7ca0e8c
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-19 17:55:45 +0900

    編集画面の作成と実装
    編集画面用のビューとサーバーサイドの実装しました。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,24 @@ class ProductsController < ApplicationController
       render :new
     end
   end
+  def edit
+    @product = Product.find(params[:id])  
+  end
+  def update
+    @product = Product.find(params[:id])  
+    if @product.update(update_params) 
+      # && @category.update(category_params) && @status.update(status_params) && @brand.update(brand_params)
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end  
+  def destroy
+    @product = Product.find(params[:id])  
+    if @product.destroy 
+      redirect_to action: :index
+    end
+  end
 
   private
   def create_params
@@ -45,5 +63,14 @@ class ProductsController < ApplicationController
       brand_attributes: [:id, :name],
       images_attributes: [:id, :image_url]
     )
+  end
+  def update_params
+    params.require(:product).permit(
+      :name, :price, :delivery, :description, :exposition, :delivery_fee, :shipping_area, :shipping_days, :saller_id, :buyer,
+     category_attributes: [:id, :name],
+     status_attributes: [:id, :name],
+     brand_attributes: [:id, :name],
+     images_attributes: [:id, :image_url]
+   )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
   
-  resources :products, only: [:index, :show, :new, :create] do
+  resources :products, only: [:index, :show, :new, :create,:destroy,:update,:edit] do
     collection do
       get '/buy/:id'  => 'products#buy'
       get '/buyer/:id' => 'products#buyer'
+      get '/destroy/:id'  => 'products#destroy'
     end
   end
   
