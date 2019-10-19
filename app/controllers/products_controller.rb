0aeb596225de708e782518e02bbbd03211ecd507
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-19 18:24:33 +0900

    レンダーとコントローラーの記述の修正
    コメントアウトやスペース、レンダーの微調整

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,7 +41,6 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])  
     if @product.update(update_params) 
-      # && @category.update(category_params) && @status.update(status_params) && @brand.update(brand_params)
       redirect_to action: :index
     else
       render :new
@@ -58,19 +57,19 @@ class ProductsController < ApplicationController
   def create_params
     params.require(:product).permit(
        :name, :price, :delivery, :description, :exposition, :delivery_fee, :shipping_area, :shipping_days, :saller_id, :buyer,
-      category_attributes: [:id, :name],
-      status_attributes: [:id, :name],
-      brand_attributes: [:id, :name],
-      images_attributes: [:id, :image_url]
+      category_attributes: [:id,:name],
+      status_attributes: [:id,:name],
+      brand_attributes: [:id,:name],
+      images_attributes: [:id,:image_url]
     )
   end
   def update_params
     params.require(:product).permit(
       :name, :price, :delivery, :description, :exposition, :delivery_fee, :shipping_area, :shipping_days, :saller_id, :buyer,
-     category_attributes: [:id, :name],
-     status_attributes: [:id, :name],
-     brand_attributes: [:id, :name],
-     images_attributes: [:id, :image_url]
+     category_attributes: [:id,:name],
+     status_attributes: [:id,:name],
+     brand_attributes: [:id,:name],
+     images_attributes: [:id,:image_url]
    )
   end
 end
