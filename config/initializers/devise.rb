6d861b9e940333381c65419efab2310c530551b6
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-20 18:39:54 +0900

    devise.rbを編集
    credential.ymlからキーを取り出すように指示

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,14 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  if Rails.env.development?
+    config.omniauth :google_oauth2,
+                    Rails.application.credentials.google[:GOOGLE_CLIENT_ID],
+                    Rails.application.credentials.google[:GOOGLE_CLIENT_SECRET]
+    config.omniauth :facebook,
+                    Rails.application.credentials.facebook[:FACEBOOK_CLIENT_ID],
+                    Rails.application.credentials.facebook[:FACEBOOK_CLIENT_SECRET]
+  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
