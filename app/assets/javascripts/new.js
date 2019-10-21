8f4192915e3fe6c30e9ff2a5a2cd2c3deabb2742
TAKAHIRO IWATA <msm_monkey@icloud.com>
2019-10-21 15:14:16 +0900

    少数切り捨て

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -3,7 +3,7 @@ $(function(){
     $('#l-left,#l-left2').empty();
     
     var Input = $('.input-default').val()
-    var Fee = $('.input-default').val()*0.1
+    var Fee = Math.floor($('.input-default').val()*0.1)
 
     if(Input >= 300 && Input <= 9999999){
       $('#l-left').append("¥ " + Fee);
