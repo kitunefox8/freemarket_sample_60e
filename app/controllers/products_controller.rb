3bd49cb99f86261c250f34dcf7a0e2f097f274b8
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-26 20:11:43 +0900

    product_controller編集
    子孫カテゴリのデータを取得

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,6 +32,14 @@ class ProductsController < ApplicationController
     @category_list = Category.all.where(ancestry: nil).map{|i| [i.name, i.id]}
   end
 
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
+  end
+
   def show 
     @images = @product.images
   end
