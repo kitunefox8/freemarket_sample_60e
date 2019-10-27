5cd2ee80c290c12bf3290eb0508ddb4394a76e21
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-27 12:48:16 +0900

    categoryコントローラの誤記入訂正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @products = Product.all.order("id DESC")
     if @category.has_children?
-      @products = Product.all.where(category_id: @category.descendant_ids).order("id DESC").page(params[:page]).per(10))
+      @products = Product.all.where(category_id: @category.descendant_ids).order("id DESC").page(params[:page]).per(10)
     else
       @products = Product.all.where(category_id: @category.id).order("id DESC").page(params[:page]).per(10)
     end
