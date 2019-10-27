7fa771e407cfb708b03f5d38a5a84a1550820a36
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-27 12:43:08 +0900

    categoryコントローラのページネーションの件数変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,9 +9,9 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @products = Product.all.order("id DESC")
     if @category.has_children?
-      @products = Product.all.where(category_id: @category.descendant_ids).order("id DESC").page(params[:page]).per(2)
+      @products = Product.all.where(category_id: @category.descendant_ids).order("id DESC").page(params[:page]).per(10))
     else
-      @products = Product.all.where(category_id: @category.id).order("id DESC").page(params[:page]).per(2)
+      @products = Product.all.where(category_id: @category.id).order("id DESC").page(params[:page]).per(10)
     end
   end
   
