520e65dc0a0b90c96089dc8e4ac057c3b35e50ef
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-29 16:07:53 +0900

    アバター画像の使用
    アバター画像の使用

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_10_29_054040) do
+
   create_table "brandgroups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
