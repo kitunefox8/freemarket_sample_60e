59df9c6901d8c996b7932311ef7a3b3af0f5c118
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-29 15:13:32 +0900

    エラーハンドリングの追加
    エラーハンドリングの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -59,8 +59,11 @@ class ProductsController < ApplicationController
   end
    
   def city
-    @product.update(profile_id: current_user.id)
+   if @product.update(profile_id: current_user.id)
     redirect_to buy_products_path(@product.id)
+   else 
+      redirect_to action: :index
+   end
   end
 
   def buy
