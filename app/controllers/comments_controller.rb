ac5f19ede2ea80b0709d94bd849a54747727586e
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-29 10:39:05 +0900

    if~present文に修正と改行の消去

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,12 @@
 class CommentsController < ApplicationController
   def create
       @comment = Comment.new(comment: comment_params[:comment], product_id: comment_params[:product_id], user_id: current_user.id)
-    if @comment.comment.blank?
-      redirect_to product_path(@comment.product.id)
-    else
+    if @comment.comment.present?
       @comment.save
       redirect_to product_path(@comment.product.id)
+    else
+      redirect_to product_path(@comment.product.id)
     end
-
-
-    
   end
 
   private
