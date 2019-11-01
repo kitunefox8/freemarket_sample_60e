fcecd26c0489b900a6efc31677814ba3aff35a83
Rita0702 <0702.t.kawakami@gmail.com>
2019-11-01 10:22:09 +0900

    画像のプレビューにturbolinksを追記

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,6 +1,5 @@
-$(function(){
+$(document).on("turbolinks:load", function(){ 
   $('.form').on('change', 'input[type="file"]', function(e) {
-    console.log(this);
     var file = e.target.files[0],
         reader = new FileReader(),
         previewArea = $('.preview');
