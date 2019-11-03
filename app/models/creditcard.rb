94be3a7b37d7ab6c2fba43a4813468271df1d959
Rita0702 <0702.t.kawakami@gmail.com>
2019-11-03 16:40:40 +0900

    creditcardモデルのバリデーション編集

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,3 +1,8 @@
 class Creditcard < ApplicationRecord
   belongs_to :user, optional: true
+  creditcard_check = /\A[0-9]+\z/
+  validates :credit_number, presence: true, length: { maximum: 20 }, format: { with: creditcard_check }, on: :create
+  validates :validity_month, presence: true, on: :create
+  validates :validity_day, presence: true, on: :create
+  validates :security_number, presence: true, length: { maximum: 4 }, format: { with: creditcard_check }, on: :create
 end
