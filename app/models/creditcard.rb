e3d600e9b37efbb72e31f0e0351379ad5966f506
Rita0702 <0702.t.kawakami@gmail.com>
2019-11-03 19:07:47 +0900

    上に同じ

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,5 +1,6 @@
 class Creditcard < ApplicationRecord
   belongs_to :user, optional: true
+
   creditcard_check = /\A[0-9]+\z/
   validates :credit_number, presence: true, length: { maximum: 20 }, format: { with: creditcard_check }, on: :create
   validates :validity_month, presence: true, on: :create
