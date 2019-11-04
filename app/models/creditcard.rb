1868b859dbba6515189e4ebb27a66efbe5cfb77f
Rita0702 <0702.t.kawakami@gmail.com>
2019-11-04 10:03:39 +0900

    新規会員登録、本番環境バリデーションエラーのためコメントアウト
    userモデルのpasswordのformat削除
    creditcardモデルの一部バリデーション解除

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -3,7 +3,7 @@ class Creditcard < ApplicationRecord
 
   creditcard_check = /\A[0-9]+\z/
   validates :credit_number, presence: true, length: { maximum: 20 }, format: { with: creditcard_check }, on: :create
-  validates :validity_month, presence: true, on: :create
-  validates :validity_day, presence: true, on: :create
+  # validates :validity_month, presence: true, on: :create
+  # validates :validity_day, presence: true, on: :create
   validates :security_number, presence: true, length: { maximum: 4 }, format: { with: creditcard_check }, on: :create
 end
